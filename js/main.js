09a8679f7dd10de6f17d676f7052a4e487b25d55
krithicktony <toinboxofkrithick@outlook.com>
2024-12-31 06:52:21 -0500

    clixcrm

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  function showslider(){
+    const slides = document.querySelector('.navright')
+    slides.style.display = 'flex'
+    event.preventDefault();
+}
+function hideslider(){
+    const slides = document.querySelector('.navright')
+    slides.style.display = 'none'
+    event.preventDefault();
+}
